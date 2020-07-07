5c341a3b49409912e1a3e794f22b143914c38592
Paul Coroneos <paul@pcoroneos.com>
2020-07-06 21:29:45 -0500

    complete exercise

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+function init(){
+  const p = document.querySelector('p');
+  console.log(p);
+  const imgs = document.querySelectorAll('img');
+  console.log(imgs);
+}
+
+document.addEventListener('DOMContentLoaded', init)
